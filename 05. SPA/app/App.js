cab02e5f6b9a6effbb9ad45c82a9ceb9361e66d4
INRIAS <inrialva@gmail.com>
2025-10-28 22:17:17 -0500

    Helpers And Components

--- 05. SPA/app/App.js
+++ 05. SPA/app/App.js
@@ -1,10 +1,26 @@
 import API from "./helpers/wp_api.js";
+import { ajax } from "./helpers/ajax.js";
+import { Title } from "./components/Title.js";
 
 const d = document;
 
 export default function App() {
-    // d.getElementById("root").innerHTML= `<h1>Prueba root</h1>`
-    
-    console.log(API);
-    
+  const d = document,
+  $root = d.getElementById("root");
+
+  $root.appendChild(Title())
+  ajax({
+    url: API.POSTS,
+    cbSuccess: (posts) => {
+      console.log(posts);
+    },
+  });
+
+  ajax({
+    url: API.CATEGORIES,
+    cbSuccess:(categories)=>{
+        console.log(categories);
+        
+    }
+  });
 }
